55dea47e0a88b347ddfc77b2c7e84a31800027da
Kaleab Solomon <kaleabsolomon98@gmail.com>
2025-06-14 22:24:35 +0300

    Weather service bug fix

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,6 +1,10 @@
+// dart imports
 import 'dart:convert';
+
+// package imports
 import 'package:http/http.dart' as http;
 
+// project imports
 import '../exceptions/network_exception.dart';
 import '../exceptions/not_found_exception.dart';
 import '../model/weather_model.dart';
@@ -10,7 +14,7 @@ class WeatherService {
     const apiKey = '532265cb6a7db2afd1ea14fc6f1200ce';
     final result = await http.get(
       Uri.parse(
-       'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey',
+        'https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=$apiKey',
       ),
     );
 
