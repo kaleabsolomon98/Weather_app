4a2d785c87f9f54b091b769ba5363c796ab25885
Kaleab Solomon <kaleabsolomon98@gmail.com>
2025-06-15 19:52:51 +0300

    Fixing issues

--- lib/pages/search_page/search_page_widgets/search_page_content_regular.dart
+++ lib/pages/search_page/search_page_widgets/search_page_content_regular.dart
@@ -1,13 +1,14 @@
-import 'package:auto_size_text/auto_size_text.dart';
+// flutter imports
 import 'package:flutter/material.dart';
 
+// project imports
+import 'package:auto_size_text/auto_size_text.dart';
+
+// project imports
 import 'city_form_field.dart';
 
 class SearchPageContentRegular extends StatelessWidget {
-  const SearchPageContentRegular({
-    super.key,
-    required this.searchController,
-  });
+  const SearchPageContentRegular({super.key, required this.searchController});
 
   final TextEditingController searchController;
 
@@ -17,35 +18,21 @@ class SearchPageContentRegular extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          const SizedBox(
-            height: 50,
-          ),
+          const SizedBox(height: 50),
           Image.asset(
             'assets/weather_logo.png',
             width: MediaQuery.of(context).size.width / 2,
           ),
-          const SizedBox(
-            height: 50,
-          ),
+          const SizedBox(height: 50),
           const AutoSizeText(
             'Search Weather',
             textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 30,
-              fontWeight: FontWeight.bold,
-            ),
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             maxLines: 2,
           ),
-          const SizedBox(
-            height: 20,
-          ),
-          CityFormField(
-            searchController: searchController,
-            textFontSize: 15,
-          ),
-          const SizedBox(
-            height: 20,
-          ),
+          const SizedBox(height: 20),
+          CityFormField(searchController: searchController, textFontSize: 15),
+          const SizedBox(height: 20),
         ],
       ),
     );
