f30f2fe3144d9a3ee90a1fd51fa8fe242bfab167
Kaleab Solomon <kaleabsolomon98@gmail.com>
2025-06-16 23:47:00 +0300

    Fixing bugs

--- lib/pages/data_page/data_page_widgets_simplified/WindSpeedSimplefied.dart
+++ lib/pages/data_page/data_page_widgets_simplified/WindSpeedSimplefied.dart
@@ -1,21 +1,17 @@
+// flutter imports
 import 'package:flutter/material.dart';
 
 class WindSpeedSimplefied extends StatelessWidget {
-  const WindSpeedSimplefied({
-    super.key,
-    required this.speed,
-  });
+  const WindSpeedSimplefied({super.key, required this.speed});
 
   final double speed;
 
   @override
   Widget build(BuildContext context) {
     return ConstrainedBox(
-      constraints: const BoxConstraints(
-        maxWidth: 400,
-      ),
+      constraints: const BoxConstraints(maxWidth: 400),
       child: Padding(
-        padding: const EdgeInsets.only(left:20,top: 5,right: 20),
+        padding: const EdgeInsets.only(left: 20, top: 5, right: 20),
         child: Row(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
