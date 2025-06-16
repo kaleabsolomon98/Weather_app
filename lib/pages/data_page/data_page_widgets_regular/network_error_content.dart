a3e7b6d8c7c6729d8b23cb1ae7ee8ef577496c7b
Kaleab Solomon <kaleabsolomon98@gmail.com>
2025-06-16 23:51:33 +0300

    Refactoring network error content

--- lib/pages/data_page/data_page_widgets_regular/network_error_content.dart
+++ lib/pages/data_page/data_page_widgets_regular/network_error_content.dart
@@ -1,10 +1,11 @@
+// flutter imports
 import 'package:flutter/material.dart';
+
+// project imports
 import 'weather_data_back_button.dart';
 
 class NetworkErrorContent extends StatelessWidget {
-  const NetworkErrorContent({
-    super.key,
-  });
+  const NetworkErrorContent({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,19 +16,13 @@ class NetworkErrorContent extends StatelessWidget {
           'assets/cloud_questionmark.png',
           width: MediaQuery.of(context).size.width / 2,
         ),
-        const SizedBox(
-          height: 30,
-        ),
+        const SizedBox(height: 30),
         const Text(
           "It's our fault, not yours.\nCome back later.",
           textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 20,
-          ),
+          style: TextStyle(fontSize: 20),
         ),
-        const SizedBox(
-          height: 40,
-        ),
+        const SizedBox(height: 40),
         const WeatherDataBackButton(),
       ],
     );
