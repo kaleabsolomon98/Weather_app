2db5fccdc8fe4102c6afd644d037b8215201010d
Kaleab Solomon <kaleabsolomon98@gmail.com>
2025-06-18 20:25:59 +0300

    bug fix

--- lib/pages/data_page/data_page_widgets_simplified/VisibilitySimplified.dart
+++ lib/pages/data_page/data_page_widgets_simplified/VisibilitySimplified.dart
@@ -1,27 +1,23 @@
+// flutter imports
 import 'package:flutter/material.dart';
 
 class VisibilitySimplified extends StatelessWidget {
-  const VisibilitySimplified({
-    super.key,
-    required this.visibility,
-  });
+  const VisibilitySimplified({super.key, required this.visibility});
 
   final int visibility;
 
   @override
   Widget build(BuildContext context) {
     return ConstrainedBox(
-      constraints: const BoxConstraints(
-        maxWidth: 400,
-      ),
+      constraints: const BoxConstraints(maxWidth: 400),
       child: Padding(
-        padding: const EdgeInsets.only(left:20,top: 5,right: 20),
+        padding: const EdgeInsets.only(left: 20, top: 5, right: 20),
         child: Row(
           children: [
             Expanded(
               child: Text(
-                "Visibility: ${visibility/1000} Km",
-               // textAlign: TextAlign.center,
+                "Visibility: ${visibility / 1000} Km",
+                // textAlign: TextAlign.center,
                 style: const TextStyle(
                   fontSize: 20,
                   //fontWeight: FontWeight.bold,
